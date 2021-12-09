bf68ce0e578b01afbfa51b07abeb529aeffee696
Omar Elmasri <masri.omarm@gmail.com>
2021-12-09 20:57:51 +0200

    get the nth node function tested

--- Unit4/lesson1/LinkedList.c
+++ Unit4/lesson1/LinkedList.c
@@ -17,15 +17,17 @@
 #include "stdint.h"
 #include "stdlib.h"
 
-//effective data
+#define BUF		40		///< capture input string
+
+///effective data
 struct Sdata
 {
 	int ID;
-	char name[40];
+	char name[BUF];
 	float height;
 };
 
-//linked list node
+///linked list node
 struct Sstudent
 {
 	struct Sdata student;
@@ -34,25 +36,71 @@ struct Sstudent
 
 #define ALLOC_FAIL		printf("\n\t\tmemory allocation failed... exit"); return 0;
 
-struct Sstudent* gpFirstStudent = NULL; //list head, only change value at addstudent first call.
-struct Sstudent* pLastStudent = NULL; // end of list, its pointer should always point to null.
+struct Sstudent* gpFirstStudent = NULL;		///list head, only change value at addstudent first call.
+struct Sstudent* pLastStudent = NULL;		/// end of list, its pointer should always point to null.
 
-// [w]:get the nth node
+#define IsListEmpty		if( gpFirstStudent == NULL ){printf("Empty list...");return 0;}
 
+inline int ListEmpty (struct Sstudent* head)
+{
+	if( gpFirstStudent == NULL )
+	{
+		printf("Empty list...");
+		return 1;
+	}
+	else return 0;
+}
+
+
+// [d]:get the nth node
 int findIndex()
 {
 	/// request node index
-	char temp [40];
-	unsigned int index;
+	char temp [BUF];
+	unsigned int index=1;
 
 	printf("\n\t Enter index to be found: ");
-	gets(temp);
+	fgets(temp,BUF,stdin);
 	index=atoi(temp);
+	if (index<1)
+	{
+		printf("\nError... Index has to be >= 1\n");
+		return 0;
+	}
+	IsListEmpty
 
 	/// searching for node index
+	struct Sstudent* tempptr= gpFirstStudent; // used to navigate through the list
+
+	if (index==1)
+	{
+	printf("\n ID: %d", tempptr->student.ID);
+	printf("\n name: %s", tempptr->student.name);
+	printf("\n height: %0.2f", tempptr->student.height);
+	return 1;
+
+	}
+	for(int tempind=2; tempind<=index; tempind++)
+	{
+		tempptr=tempptr->pNextStudent;
+		if (tempptr->pNextStudent== NULL && tempind!=index )
+		{
+			printf("\nIndex exceeds List");
+			return 0;
+		}
+	}
+	printf("\n ID: %d", tempptr->student.ID);
+	printf("\n name: %s", tempptr->student.name);
+	printf("\n height: %0.2f", tempptr->student.height);
+	return 1;
+
 }
 
-// [p]:count number of node
+// [w]:count number of node
+int CountNodes()
+{
+	
+}
 
 // [p]:get the nth node from the end.
 // use 2 ptr with detr space = nth node
@@ -78,7 +126,7 @@ inline int alloc_check( void* ptr )
 int AddStuden()
 {
 	struct Sstudent* pNewStudent;
-	char temp [40];
+	char temp [BUF];
 	if ( gpFirstStudent == NULL ) //	incase of first entry
 	{
 		//create new record
@@ -89,7 +137,8 @@ int AddStuden()
 		// }
 		//malloc success, make head points to the first record of the list
 		gpFirstStudent = pNewStudent; //now head points to first entry
-		pLastStudent = pNewStudent; // first and last entry in the list
+		pLastStudent = pNewStudent; // first and last entry in the 
+		pNewStudent->pNextStudent=NULL;
 		pLastStudent->pNextStudent = NULL; //closing the list
 
 	}
@@ -104,31 +153,30 @@ int AddStuden()
 			ALLOC_FAIL
 		}
 	}
-		pLastStudent->pNextStudent = pNewStudent;	//linking last(old entry) entry to the new entry
+		pLastStudent->pNextStudent= pNewStudent;	//linking last(old entry) entry to the new entry
+		pNewStudent->pNextStudent=NULL;
+		pLastStudent=pNewStudent;
 		// release(point to null) last data pointer after filling data of new entry
 		// filling data
 		printf("\n Enter the ID: ");
-		gets(temp);
+		fgets(temp,BUF,stdin);
 		pNewStudent->student.ID = atoi(temp);
 		printf("\n Enter the full name: ");
 		gets(pNewStudent->student.name);
 		printf("\n Enter the height: ");
-		gets(temp);
+		fgets(temp,BUF,stdin);
 		pNewStudent->student.height = atof(temp);
-
-		pNewStudent->pNextStudent = NULL; 	 //closing the list
-		pLastStudent = pNewStudent; 		//end of the list
 
 		return 0;
 }
 
 int DelStudent ()
 {
-	char temp [40];
+	char temp [BUF];
 	unsigned int idtemp;
 
 	printf("\n\t Enter student id to be deleted: ");
-	gets(temp);
+	fgets(temp,BUF,stdin);
 	idtemp=atoi(temp);
 
 	if( gpFirstStudent == NULL )
@@ -196,7 +244,7 @@ void DelAll()
 
 void main()
 {
-	char temp_text[40];
+	char temp_text[BUF];
 	char loopflag = 1; char exitconfirm;
 
 	while (loopflag)
@@ -207,7 +255,8 @@ void main()
 		printf("\n\t 2: Delete Student");
 		printf("\n\t 3: View Student");
 		printf("\n\t 4: Delete All Students");
-		printf("\n\t 5: Exit");
+		printf("\n\t 5: Find Index");
+		printf("\n\t 6: Exit");
 		printf("\n\t Enter Option Number: ");
 
 		gets(temp_text);
@@ -227,6 +276,9 @@ void main()
 			ViewStudent();
 			break;
 			case 5:
+			findIndex();
+			break;
+			case 6:
 			printf("Confirm Exit? [y/n]");
 			scanf("%c",&exitconfirm);
 			switch(exitconfirm)
