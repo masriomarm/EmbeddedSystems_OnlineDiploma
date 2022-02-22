7b6595287aaaab51c243e3c536fe53cba37f4156
Omar Elmasri <masri.omarm@gmail.com>
2022-02-22 20:58:24 +0200

    starting project 1, unit 5

---
+++ Unit4/lesson3/STM32F103C8-BluePill/mainAlg.c
@@ -0,0 +1,19 @@
+/**
+ * @file mainAlg.c
+ * @author Omar Elmasri (masri.omarm@gmail.com)
+ * @brief Pressure alarm system. @n
+ * Embedded systems diploma. Learn in depth.
+ *
+ * @details System shall trigger an alarm upon exceeding pressure threshold. @n@n
+ * System shall read pressure value using Pressure sensor circuit provided by the user.@n
+ * System shall compare pressure value to hard-coded pressure threshold provided by the user.@n
+ * System shall generate an alarm signal to drive alarm circuit provided by the user.@n
+ * System shall maintain alarm signal for hard-coded period provided by the user.@n
+ * System shall reset alarm value only after the alarm period is over and pressure is below threshold.@n
+ *
+ * @date 2022-02-21
+ * @version 0.1
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
