cc088c9977f9737e2c409d112c3d23e3f029fb16
Omar Elamsri <masri.omarm@gmail.com>
2022-07-07 13:32:54 +0200

    migrate lcd functions to mcal driver

--- Unit7/section3/src/lcd.c
+++ Unit7/section3/src/lcd.c
@@ -7,49 +7,75 @@
 
 #include "inc/lcd.h"
 #include "inc/generic.h"
+#include "inc/stm32f103c6_GPIO.h"
 
-void init_LCD(void) {
+void init_LCD(void)
+{
   _delay_ms(20);
   LCD_cmd(LCD_CMD_8_BIT_MODE);
   LCD_cmd(LCD_CMD_DISP_ON_CURS_BLNK);
   LCD_cmd(LCD_CMD_DISP_CLR);
   _delay_us(2000);
 }
-void LCD_cmd(uint8_t cmd) {
-  CLR_BIT(PORTB, LCD_RS);
-  CLR_BIT(PORTB, LCD_RW);
-  _delay_us(10);
-  PORTA = cmd;
-  SET_BIT(PORTB, LCD_EN);
-  _delay_us(1);
-  CLR_BIT(PORTB, LCD_EN);
+void LCD_cmd(uint8_t cmd)
+{
+  //   CLR_BIT(PORTB, LCD_RS);
+  MCAL_GPIO_write_pin(LCD_PORT, LCD_RS, GPIO_PIN_RESET);
+  //  CLR_BIT(PORTB, LCD_RW);
+  MCAL_GPIO_write_pin(LCD_PORT, LCD_RW, GPIO_PIN_RESET);
+  _delay_ms(10);
+
+  uint16_t temp = MCAL_GPIO_read_port(LCD_PORT);
+  temp &= 0xFF00;
+  temp |= ((uint16_t)(cmd));
+  MCAL_GPIO_write_port(LCD_PORT, temp);
+
+  //  SET_BIT(PORTB, LCD_EN);
+  MCAL_GPIO_write_pin(LCD_PORT, LCD_EN, GPIO_PIN_SET);
+  _delay_ms(1);
+  //  CLR_BIT(PORTB, LCD_EN);
+  MCAL_GPIO_write_pin(LCD_PORT, LCD_EN, GPIO_PIN_RESET);
   _delay_us(100);
 }
-void LCD_disp(char data) {
+void LCD_disp(char data)
+{
   static int pos = 0;
-  if (pos < 16) {
+  if (pos < 16)
+  {
     LCD_disp_char(data);
     pos++;
-  } else if (pos == 16) {
+  }
+  else if (pos == 16)
+  {
     LCD_cmd(LCD_CMD_CURS_LINE_2);
     LCD_disp_char(data);
     pos++;
-  } else if (pos < 32) {
+  }
+  else if (pos < 32)
+  {
     LCD_disp_char(data);
     pos++;
-  } else {
+  }
+  else
+  {
     LCD_cmd(LCD_CMD_DISP_CLR);
     LCD_cmd(LCD_CMD_CURS_LINE_1);
     LCD_disp_char(data);
     pos = 1;
   }
 }
-void LCD_disp_char(char data) {
-  PORTA = data;
-  SET_BIT(PORTB, LCD_RS);
-  CLR_BIT(PORTB, LCD_RW);
-  SET_BIT(PORTB, LCD_EN);
-  _delay_us(1);
-  CLR_BIT(PORTB, LCD_EN);
+void LCD_disp_char(char data)
+{
+  //  GPIOA = data;
+  uint16_t temp = MCAL_GPIO_read_port(LCD_PORT);
+  temp &= 0xFF00;
+  temp |= (data);
+  MCAL_GPIO_write_port(LCD_PORT, temp);
+
+  MCAL_GPIO_write_pin(LCD_PORT, LCD_RS, GPIO_PIN_SET);
+  MCAL_GPIO_write_pin(LCD_PORT, LCD_RW, GPIO_PIN_RESET);
+  MCAL_GPIO_write_pin(LCD_PORT, LCD_EN, GPIO_PIN_SET);
+  _delay_ms(1);
+  MCAL_GPIO_write_pin(LCD_PORT, LCD_EN, GPIO_PIN_RESET);
   _delay_us(100);
 }
