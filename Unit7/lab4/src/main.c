89ba202b90f0bdeb8825a0e7a284d12cbf7c68b6
Omar Elmasri <masri.omarm@gmail.com>
2022-07-13 00:36:27 +0200

    LAB 4 application

--- Unit7/lab4/src/main.c
+++ Unit7/lab4/src/main.c
@@ -11,16 +11,37 @@
 #include "inc/stm32f103c6_EXTI.h"
 
 void init_GPIO(void);
+void callback_exti9(void)
+{
+  LCD_cmd(LCD_CMD_DISP_CLR);
+  char msg[] = "hello from exti9";
+  int  index = 0;
+  while (msg[index++] != '\0')
+  {
+    LCD_disp(msg[index]);
+  }
+}
 
 int main()
 {
-
-  EXTI_GPIO_Mapping_t temp __attribute__((unused));
-  temp = EXTI1PA1;
+#define LCD
+#define LAB_4
   CLK_EN_GPIOA;
   CLK_EN_GPIOB;
+  CLK_EN_AFIO;
   init_GPIO();
   init_LCD();
+#ifdef LAB_4
+  EXTI_GPIO_Mapping_t temp __attribute__((unused));
+
+  temp                       = EXTI9PA9;
+  EXTI_PinConfig_t temp_cnfg = {.EXTI_Line        = temp,
+                                .EXTI_TriggerEdge = EXTI_Trigger_Rising,
+                                .IRQ_EN           = EXTI_IRQ_Enable,
+                                .EXTI_CallBack    = callback_exti9};
+  MCAL_EXTI_GPIO_Init(&temp_cnfg);
+#endif
+#ifdef SECTION_3
   Keypad_init();
   char flag = 1;
   char key;
@@ -46,11 +67,13 @@ int main()
       flag = 0;
     }
   }
+#endif
   return 0;
 }
 
 void init_GPIO(void)
 {
+#ifdef LCD
   /// init LCD pins
   {
     GPIO_PinConfig_t cnfg = {.GPIO_Port  = LCD_PORT,
@@ -77,7 +100,9 @@ void init_GPIO(void)
     }
   }
   //  DDRB |= (0b111 << 0);
+#endif
 
+#ifdef KEYPAD
   /// init Keypad pins
   {
     GPIO_PinConfig_t cnfg = {.GPIO_Port  = KEY_PAD_PORT,
@@ -101,4 +126,5 @@ void init_GPIO(void)
     }
   }
   //  DDRD = 0x00;
+#endif
 }
